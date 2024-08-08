3cc9deb4340e49119e61a4d16a9353bdf9e06e58
Shai Komisar <kofmisar@gmail.com>
2024-08-08 17:46:33 +0300

    Flights API Version 1.0

---
+++ accessories.mjs
@@ -0,0 +1,94 @@
+const estimateDeparture = "CHSTOL"
+const realDeparture = "CHPTOL"
+const flightCode = "CHOPER"
+const flightNumber = "CHFLTN"
+const checkInCounter = "CHCINT"
+const countryName = "CHLOCCT"
+const cityName = "CHLOC1T"
+
+function isOutbound(val) {
+    return val[checkInCounter] == null
+}
+
+function isInbound(val) {
+    return !isOutbound(val)
+}
+
+function isDelayed(val) {
+    const estimateTime = Date.parse(val[estimateDeparture])
+    const actualTime = Date.parse(val[realDeparture])
+
+    return actualTime > estimateTime
+}
+
+export async function getData() {
+    const resource_id = "e83f763b-b7d7-479e-b172-ae981ddc6de5"
+    const max_size = 300
+    const data = `https://data.gov.il/api/3/action/datastore_search?resource_id=${resource_id}&limit=${max_size}`
+    return fetch(data)
+        .then(response => response.json())
+        .then(response => response.result.records)
+}
+
+export function getAmount(res) {
+    return res.length
+}
+
+export function filterOutbound(res) {
+    return res.filter(isOutbound)
+}
+
+export function filterInbound(res) {
+    return res.filter(isInbound)
+}
+
+export function filterCountry(res, country) {
+    return res.filter(val => val[countryName] == country)
+}
+
+export function filterDelayed(res) {
+    return res.filter(isDelayed)
+}
+
+export function findPopulariest(res) {
+    let cities = {}
+    let max = 0
+    let maxCity = ""
+
+    for (let val of res) {
+        let city = val[cityName]
+        if (!(city in cities)) {
+            cities[city] = 0
+        }
+        cities[city]++
+    }
+
+    for (let key in cities) {
+        if (cities[key] > max) {
+            maxCity = key
+            max = cities[key]
+        }
+    }
+
+    return maxCity
+}
+
+export function findQuickGateway(response, res) {
+    const inbound = filterInbound(res)
+    const outbound = filterOutbound(res)
+    const earliestInbound = inbound.reduce((a, b) => Date.parse(a[estimateDeparture]) < Date.parse(b[estimateDeparture]) ? a : b)
+    const latestOutbound = outbound.reduce((a, b) => Date.parse(a[estimateDeparture]) > Date.parse(b[estimateDeparture]) ? a : b)
+
+    if (earliestInbound[estimateDeparture] > latestOutbound[estimateDeparture]) {
+        response.status(406).send('Error 406: No result was found')
+        return
+    }
+
+    console.log(earliestInbound)
+    console.log(latestOutbound)
+
+    return {
+        departure: latestOutbound[flightCode] + latestOutbound[flightNumber],
+        arrival: earliestInbound[flightCode] + earliestInbound[flightNumber],
+    }
+}
---
+++ index.js
@@ -0,0 +1,97 @@
+import express from 'express'
+import { readFile } from 'fs/promises'
+import { filterCountry, filterDelayed, filterInbound, filterOutbound, findPopulariest, findQuickGateway, getAmount, getData } from './accessories.mjs'
+
+
+async function mainPage(_, response) {
+    response.send(await readFile('./home.html', 'utf8'))
+}
+
+function amountFlight(_, response) {
+    getData()
+        .then(getAmount)
+        .then(res => response.json(res))
+        .catch(error => console.log("Error: ", error))
+}
+
+function amountOutboundFlight(_, response) {
+    getData()
+        .then(filterOutbound)
+        .then(getAmount)
+        .then(res => response.json(res))
+        .catch(error => console.log("Error: ", error))
+}
+
+function amountInboundFlight(_, response) {
+    getDataata()
+        .then(filterInbound)
+        .then(getAmount)
+        .then(res => response.json(res))
+        .catch(error => console.log("Error: ", error))
+}
+
+function amountCountryFlight(request, response) {
+    const country = request.query.country
+    getData()
+        .then(res => filterCountry(res, country))
+        .then(getAmount)
+        .then(res => response.json(res))
+        .catch(error => console.log("Error: ", error))
+}
+
+function amountCountryOutboundFlight(request, response) {
+    const country = request.query.country
+    getData()
+        .then(res => filterCountry(res, country))
+        .then(filterOutbound)
+        .then(getAmount)
+        .then(res => response.json(res))
+        .catch(error => console.log("Error: ", error))
+}
+
+function amountCountryInboundFlight(request, response) {
+    const country = request.query.country
+    getData()
+        .then(res => filterCountry(res, country))
+        .then(filterInbound)
+        .then(getAmount)
+        .then(res => response.json(res))
+        .catch(error => console.log("Error: ", error))
+}
+
+function amountDelayedFlight(_, response) {
+    getData()
+        .then(filterDelayed)
+        .then(getAmount)
+        .then(res => response.json(res))
+        .catch(error => console.log("Error: ", error))
+}
+
+function mostPopularCity(_, response) {
+    getData()
+        .then(findPopulariest)
+        .then(res => response.json(res))
+        .catch(error => console.log("Error: ", error))
+}
+
+function quickGetaway(_, response) {
+    getData()
+        .then(res => findQuickGateway(response, res))
+        .then(res => response.json(res))
+        .catch(error => console.log("Error: ", error))
+}
+
+
+const app = express();
+app.get('/', mainPage)
+app.get('/amountFlight', amountFlight)
+app.get('/amountOutboundFlight', amountOutboundFlight)
+app.get('/amountInboundFlight', amountInboundFlight)
+app.get('/amountCountryFlight', amountCountryFlight)
+app.get('/amountCountryOutboundFlight', amountCountryOutboundFlight)
+app.get('/amountCountryInboundFlight', amountCountryInboundFlight)
+app.get('/amountDelayedFlight', amountDelayedFlight)
+app.get('/mostPopularCountry', mostPopularCity)
+app.get('/quickGetaway', quickGetaway)
+
+app.listen(process.env.PORT || 3000, () => console.log('App available on localhost:3000'))
